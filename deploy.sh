c1530503cf511872a48b24ab74cc47ebfa4e72e1
Mark Stouffer <=>
2018-03-22 19:48:58 -0700

    rebuilding site Thu, Mar 22, 2018  7:48:54 PM

--- deploy.sh
+++ deploy.sh
@@ -13,7 +13,7 @@ printf "Build the project."
 rmdir docs/** -r
 rm -rf docs/** 
 
-printf "phi-fi.com" > docs/CNAME
+# printf "phi-fi.com" > docs/CNAME
 hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
 
 msg="rebuilding site `date`"
